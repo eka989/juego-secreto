3421b38a83456a1fcac4a69ca53d23315952b57c
eka989 <eugeniotobartortora@gmail.com>
2025-01-20 02:58:28 -0300

    Update app.js
    Se agrega pantalla final

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ let intentos = 0;
 let listaNumerosSorteados = [];
 let numeroMaximo = 10;
 
+
 function asignarTextoElemento (elemento, texto) {
   let elementoHTML = document.querySelector(elemento); //titulo no es texto, es un objeto
   elementoHTML.innerHTML = texto;
   return; //no es necesario
 }
+
 
 function verificarIntento() {
   let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value); //obtenemos el valor del input, es diferente de obtener EL elemento "input"
@@ -37,6 +39,7 @@ function verificarIntento() {
   return;
 }
 
+
 function generarNumeroSecreto() {
   let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1; //nos evitamos generar una variable que almacene el numero secreto
   console.log(listaNumerosSorteados);
@@ -45,6 +48,9 @@ function generarNumeroSecreto() {
   if (listaNumerosSorteados.length == numeroMaximo) {
     asignarTextoElemento('h1', '¡Final!');
     asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles.');
+    document.getElementById('reiniciar').style.display = 'none';
+    document.getElementById('intentar').style.display = 'none';
+    document.getElementById('valorUsuario').style.display = 'none';
     return;
   }
   //si el numero generado ya esta en la lista, generamos otro numero
@@ -56,10 +62,12 @@ function generarNumeroSecreto() {
   }
 }
 
+
 function limpiarCaja () {
   let valorCaja = document.querySelector('#valorUsuario').value = '';//no es necesario el querySelector, se puede hacer directamente con el id
   valorCaja.value = ''; //vacio
 }
+
 
 function condicionesIniciales() {
   asignarTextoElemento ('h1', 'Juego del Número Secreto');
@@ -68,6 +76,7 @@ function condicionesIniciales() {
   intentos = 1
 }
 
+
 function reiniciarJuego() {
   //limpiar la caja, mensaje 1 a 10, generar nuevo num aleatorio, deshabilitar boton, reiniciar intentos
   limpiarCaja();
@@ -75,5 +84,6 @@ function reiniciarJuego() {
   condicionesIniciales();
   document.getElementById('reiniciar').setAttribute('disabled', 'true');//pide 2 parametros, el atributo y el valor, el remove no necesita valor
 }
+
 
 reiniciarJuego(); //inicializar el juego
